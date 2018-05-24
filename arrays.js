e72abcd41423c3e7b0ee3aecbd348b60946771f4
jmontenegro@uh.edu <jmontenegro@uh.edu>
2018-05-24 05:20:02 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -48,7 +48,7 @@ function destructivelyRemoveElementFromEndOfArray(array)
 
 function removeElementFromEndOfArray(array)
 {
-  array.slide(array.length-1)
+  array.slice(array.length-1)
   return array
 }
 
