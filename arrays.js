7684a9daf419a86424df8a2de5961ae7222b1721
jmontenegro@uh.edu <jmontenegro@uh.edu>
2018-05-24 05:10:48 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -10,3 +10,10 @@ function destructivelyAddElementToBeginningOfArray( array, element)
   array.unshift(element)
   return array
 }
+
+function addElementToEndOfArray(array, element)
+{
+  return ([array,...element])
+}
+
+function 
