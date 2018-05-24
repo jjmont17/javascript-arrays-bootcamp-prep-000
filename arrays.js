0bae1f60b8d04b29b4751b52accae7c6f3c8c161
jmontenegro@uh.edu <jmontenegro@uh.edu>
2018-05-24 05:07:52 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -2,6 +2,6 @@ var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
 
 function addElementToBeginningOfArray(array, element)
 {
-  return([element,...array])
+  return array.unshift(element)
 }
 
